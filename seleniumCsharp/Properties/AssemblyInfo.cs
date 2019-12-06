bbd8c1f8bdc83b78e5778f0d55e9083116f3446e
johna <john.artur.linder@gmail.com>
2019-12-06 14:10:59 +0100

    Add project files.

---
+++ seleniumCsharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("seleniumCsharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("seleniumCsharp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("01bfa0fd-2fec-4296-9002-542b5e237413")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ seleniumCsharp/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+namespace MySeleniumProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeDriver Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://www.hm.se");
+            /*driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);*/
+            IWebElement SearchFunction = Driver.FindElementById("main-search");
+            SearchFunction.Click();
+            Driver.FindElementById("main-search").SendKeys("kofta");
+            Driver.FindElementByClassName("menu__search_submit").Click();
+            Driver.FindElementByXPath("//button[@class='close icon-close-white js-close']").Click();
+            Driver.FindElementByClassName("item-image").Click(); //första koftan
+
+            Driver.FindElementByXPath("//button[contains(@class,'picker-trigger js-picker-trigger small notify-if-back')]//span[contains(@class,'value')][contains(text(),'Välj storlek')]").Click();
+            Driver.FindElementByXPath("//ul/li[2]/div/button/span").Click(); //XS 
+                                                                             //Driver.FindElementByXPath("#main-content > div.product.parbase > div.layout.pdp-wrapper.product-detail.sticky-footer-wrapper.js-reviews > div.module.product-description.sticky-wrapper > div.sub-content.product-detail-info.inner.sticky-on-scroll.semi-sticky > div > div.product-item-buttons > div.product-button-wrapper > button").Click();
+            /*  Actions action = new Actions(Driver);
+              ChromeWebElement mobileElement = Driver.FindElementByCssSelector(".icon-shopping-bag-white");
+              Action.moveToElement(mobileElement).build().perform();*/
+            Driver.FindElementByCssSelector(".icon-shopping-bag-white").Click();
+            Driver.Close();
+            Driver.Quit();
+            /*Actions action = new Actions(Driver);
+            WebElement mobileElement = Driver.findElement(By.linkText("Mobile & Accessories"));
+            action.moveToElement(mobileElement).build().perform();*/
+        }
+    }
+}
